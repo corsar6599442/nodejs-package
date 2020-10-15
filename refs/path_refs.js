31fe862169e5eaca7f231e19e6b692a21c8d7315
Mikhail_Zakharov <corsar6599442@gmail.com>
2020-10-15 12:04:19 +0300

    Рассмотрели модуль path

--- refs/path_refs.js
+++ refs/path_refs.js
@@ -1,5 +1,13 @@
-const path = require ('path')
+const path = require ('path') //получаем объект path
 
-console.log(__filename)
-console.log(path.basename(__filename))
-console.log(path.dirname(__filename))
+console.log(__filename) //возвращает полный путь+имя файла в котором вызван
+console.log(path.basename(__filename)) //возвращает название текущего файла с расширением
+console.log(path.dirname(__filename)) //возвращает директорию в которой лежит файл
+console.log(path.extname(__filename)) //возвращает расширение файла
+
+console.log(path.parse(__filename)) //возвращает информацию о текущем файле в виде объекта
+console.log(path.parse(__filename).ext) //возвращает поле ext
+console.log(path.parse(__filename).base) //возвращает поле base и т.д.
+
+console.log(path.join(__dirname, 'test', 'second.html')) //генерирует путь к файлу second.html до текущей директории
+console.log(path.resolve(__dirname, './test', '/second.html')) // '/'-перед названием файла указывает на относительный путь
