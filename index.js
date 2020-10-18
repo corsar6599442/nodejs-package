36002244e299dd4e7b300bf347059f83d8b3ff3f
Mikhail_Zakharov <corsar6599442@gmail.com>
2020-10-18 21:40:41 +0300

    Пофиксил баги

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const server = http.createServer((req, res) => {
 			'Content-Type': 'text/html; charset=utf-8'
 		})
 		res.end(`
-	<h1>Form</h1>
-	<form method="post" action="/">
-	<input name="title" type="text" />
-	<button type="submit">Send</button
-	</form>
+			<h1>Form</h1>
+			<form method="post" action="/">
+				<input name="title" type="text" />
+				<button type="submit">Send</button>
+			</form>
 		`)
 	} else if (req.method === 'POST') {
 		const body = []
@@ -23,12 +23,11 @@ const server = http.createServer((req, res) => {
 
 		req.on('end', () => {
 			const message = body.toString().split('=')[1]
-			console.log(message)
+
+			res.end(`
+	  			<h1>Ваше сообщение:${message}</h1>
+	  		`)
 		})
-		
-	//   	res.end(`
-	//   	<h1>Ваше сообщение:${message} </h1>
-	//   	`)
     }
 })
 
